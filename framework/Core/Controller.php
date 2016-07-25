4416e6c0324ef07c5b438047ff6815a669aad4d2
brusni4ka <katerynahovor@gmail.com>
2016-07-26 02:18:57 +0300

    Update Controller.php

--- framework/Core/Controller.php
+++ framework/Core/Controller.php
@@ -3,15 +3,14 @@ namespace HappyCake\Core;
 
 class Controller
 {
-    //объект View
-    protected $view;
 
-    // protected $model;
+    protected $view;
+    //protected $model;
 
     function __construct()
     {
         $this->view = new View();
-        //   $this->model = new Model();
+      //  $this->model = new Model();
     }
 
 }
